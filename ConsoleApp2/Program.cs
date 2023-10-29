2a31011908c14e83ef8019b3be28d5bc6b4aa363
mar707usz <mariusz707@maulcom>
2023-10-29 15:01:07 +0100

    dzien 4

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+var name = "Ewa";
+var age = 33;
+var gender = "woman";
+
+if (gender == "woman" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender == "men" && age < 18)
+
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
